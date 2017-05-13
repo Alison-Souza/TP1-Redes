51ef1e5e71f48e77f8802bb92fb17665d214e1d3
Alison-Souza <alison.souza.mg@gmail.com>
2017-05-13 17:05:23 -0300

    Base pronta

--- host.c
+++ host.c
@@ -5,6 +5,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
+#include <errno.h>
 
 #define MAXDATASIZE 512
 #define SYNC 0xdcc023c2
@@ -22,6 +24,12 @@ typedef struct packet
 	uint8_t flags;
 	uint8_t data[MAXDATASIZE];
 } packet_t;
+
+//***********************************************************************//
+//						ALGORITMO DO CHECKSUM							 //
+//						Encontrado neste link:							 //
+// https://codereview.stackexchange.com/questions/154007/16-bit-checksum-function-in-c
+//***********************************************************************//
 
 unsigned short csum(unsigned short *ptr, int nbytes) 
 {
@@ -51,6 +59,9 @@ unsigned short csum(unsigned short *ptr, int nbytes)
 
 int main(int argc, char* argv[])
 {
+//***********************************************************************//
+//						CHECANDO ARGUMENTOS								 //
+//***********************************************************************//
 	if(argc < 2)
 	{
 		fprintf(stderr, "ERROR: Flags and parameters not set.\n");
@@ -75,14 +86,21 @@ int main(int argc, char* argv[])
 		}
 	}
 
+//***********************************************************************//
+//						INICIALIZANDO VARIAVEIS							 //
+//***********************************************************************//
+
 	int socketFD, newSocketFD, portNum;
 	struct sockaddr_in local_addr, remote_addr;
 	char *input = argv[3];
 	char *output = argv[4];
-	socklen_t sockSize;
-	packet_t pacote;
+	packet_t pacote, ack;
 	uint8_t last_id = 1;
 	uint16_t last_chksum = 0;
+
+//***********************************************************************//
+//						INICIALIZANDO CONEXOES							 //
+//***********************************************************************//
 
 	socketFD = socket(AF_INET, SOCK_STREAM, 0);
 	if(socketFD < 0)
@@ -91,8 +109,11 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	if(strcmp(argv[1], "-s") == 0) // Se é servidor, faz conexão passiva.
+	// SE CONEXÃO É PASSIVA (SERVIDOR - RECEPTOR).
+	if(strcmp(argv[1], "-s") == 0)
 	{
+		socklen_t sockSize;
+
 		portNum = atoi(argv[2]);
 		memset((char*) &local_addr, '\0', sizeof(local_addr));
 		local_addr.sin_family = AF_INET;
@@ -120,9 +141,11 @@ int main(int argc, char* argv[])
 			exit(1);
 		}
 	}
-	else if(strcmp(argv[1], "-c") == 0) // Se é cliente, faz conexão ativa.
+	// SE CONEXAO É ATIVA (CLIENTE - TRANSMISSOR).
+	else if(strcmp(argv[1], "-c") == 0)
 	{
 		char *ip, *port;
+
 		ip = strtok(argv[2], ":");
 		port = strtok(NULL, " ");
 		
@@ -140,99 +163,156 @@ int main(int argc, char* argv[])
 			exit(1);
 		}
 	}
-		
+
+//***********************************************************************//
+//***********************************************************************//
+//							ENVIANDO DADOS								 //
+//***********************************************************************//
+//***********************************************************************//
+	FILE *fsend = fopen(input, "r");
+	if(fsend == NULL)
+	{
+		fprintf(stderr, "ERROR: %s file cannot be opened", input);
+		fclose(fsend);
+		exit(1);
+	}
+
+	int blockSize;
+	uint8_t buffer[MAXDATASIZE];
+
+	while((blockSize = fread(buffer, sizeof(uint8_t), MAXDATASIZE, fsend)) >= 0)
+	{
+		memset(&pacote, '\0', sizeof(packet_t));
+		pacote.sync1 = htonl(SYNC);
+		pacote.sync2 = htonl(SYNC);
+		pacote.chksum = htons(0);
+		pacote.length = htons(blockSize);
+		last_id == 1 ? (pacote.id = htons(0)) : (pacote.id = htons(1));
+		blockSize == MAXDATASIZE ? (pacote.flags = htons(0)) : (pacote.flags = htons(64)); //64 = b'0100 0000
+		memcpy(pacote.data, &buffer, blockSize);
+
+		pacote.chksum = htons((uint16_t) csum((unsigned short *) &pacote, sizeof(packet_t)));
+
+		if(send(socketFD, (packet_t *) &pacote, sizeof(packet_t), 0) < 0)
+		{
+			fprintf(stderr, "ERROR on sending file. Aborted.\n");
+			exit(1);
+		}
+
+		struct timeval timeout;
+		timeout.tv_sec = 1; // segundos
+		timeout.tv_usec = 0; // microsegundos
+		setsockopt(socketFD, SOL_SOCKET, SO_RCVTIMEO, (struct timeval *) &timeout, sizeof(struct timeval));
+		if(recv(socketFD, (packet_t *) &ack, sizeof(packet_t), 0) < 0)
+		{
+			if(errno == EAGAIN || errno == EWOULDBLOCK)
+			{
+				fseek(fsend, -blockSize, SEEK_CUR);
+				continue;
+			}
+		}
+
+		ack.id = ntohs(ack.id);
+		if(ack.id != (ntohs(pacote.id)));
+		{
+			fseek(fsend, -blockSize, SEEK_CUR);
+			continue;
+		}
+
+		last_id == 1 ? (last_id = 0) : (last_id = 1);
+	}
+	printf("File sent to server");
+	fclose(fsend);
+
 //***********************************************************************//
 //***********************************************************************//
 //							RECEBENDO DADOS								 //
 //***********************************************************************//
 //***********************************************************************//
-	FILE *fp = fopen(output, "w");
-	if(fp == NULL)
+	FILE *frecv = fopen(output, "w");
+	if(frecv == NULL)
 	{
-		fprintf(stderr, "ERROR: output file cannot be opened");
-		fclose(fp);
+		fprintf(stderr, "ERROR: %s file cannot be opened. Aborted.", output);
+		fclose(frecv);
 		exit(1);
 	}
-	else
+
+	while(1)
 	{
+		bool flag_end = false;
 		memset(&pacote, '\0', sizeof(packet_t));
-		while(1)
+		if(recv(newSocketFD, (packet_t *) &pacote, sizeof(packet_t), 0) < 0)
 		{
-			bool flag_end = false;
-			if(recv(newSocketFD, (packet_t *) &pacote, sizeof(packet_t), 0) < 0)
-			{
-				// Erro de recebimento se negativo. errno é setado.
-				break;
-			}
-
-			unsigned short checksum = csum((unsigned short *)&pacote, sizeof(packet_t));
-
-			pacote.sync1 = ntohl(pacote.sync1);
-			pacote.sync2 = ntohl(pacote.sync2);
-			pacote.chksum = ntohs(pacote.chksum);
-			pacote.length = ntohs(pacote.length);
-			pacote.id = ntohs(pacote.id);
-			pacote.flags = ntohs(pacote.flags);
-			if(pacote.length > 512 || pacote.length < 0)
-			{
-				fprintf(stderr, "pacote.length esta errado\n");
-				break;
-			}
-			if(checksum != 0)
-			{
-				fprintf(stderr, "Checksum não confere, dado corronpido\n");
-				continue;
-			}
-			if(last_id == pacote.id && last_chksum == pacote.chksum)
-			{
-				printf("Retrasmissão detectada. Reinviando confirmação\n");
-				continue;
-				//Reenvia último ACK
-			}
-			else if(last_id == pacote.id)
-			{
-				printf("ID igual ao do ultimo pacote recebido.\n");
-				continue;
-			}
-
-			if(pacote.flags == 64) // 0100 0000
-			{
-				// Pacote com bit END ligado
-				flag_end = true;
-			}
-
-			last_id = pacote.id;
-			last_chksum = pacote.chksum;
-
-			int writeSize = fwrite(pacote.data, sizeof(uint8_t), pacote.length, fp);
-			if(writeSize < pacote.length)
-			{
-				fprintf(stderr, "ERROR on write file\n");
-			}
-			if(flag_end)
-				break;
-
-			packet_t ack;
-			memset(&ack, '\0', sizeof(packet_t));
-			ack.sync1 = SYNC;
-			ack.sync2 = SYNC;
-			ack.chksum = 0;
-			ack.length = 0;
-			ack.id = pacote.id;
-			ack.flags = 128; // 1000 0000
-			memset(ack.data, '\0', MAXDATASIZE);
-
-			ack.chksum = htons((uint16_t) csum((unsigned short*) &ack, sizeof(packet_t)));
-				
-			if(send(newSocketFD, (packet_t*) &ack, sizeof(packet_t), 0) < 0);
-			{
-				fprintf(stderr, "ERROR on sending ack. ID: %d\n", ack.id);
-			}
+			// Erro de recebimento se negativo. errno é setado.
+			break;
 		}
-		printf("Received from client\n");
-		fclose(fp);
-	}
 
+		unsigned short checksum = csum((unsigned short *)&pacote, sizeof(packet_t));
+
+		pacote.sync1 = ntohl(pacote.sync1);
+		pacote.sync2 = ntohl(pacote.sync2);
+		pacote.chksum = ntohs(pacote.chksum);
+		pacote.length = ntohs(pacote.length);
+		pacote.id = ntohs(pacote.id);
+		pacote.flags = ntohs(pacote.flags);
+		if(pacote.length > 512 || pacote.length < 0)
+		{
+			fprintf(stderr, "pacote.length esta errado\n");
+			break;
+		}
+		if(checksum != 0)
+		{
+			fprintf(stderr, "Checksum não confere, dado corronpido\n");
+			continue;
+		}
+		if(last_id == pacote.id && last_chksum == pacote.chksum)
+		{
+			printf("Retrasmissão detectada. Reinviando confirmação\n");
+			continue;
+			//Reenvia último ACK
+		}
+		else if(last_id == pacote.id)
+		{
+			printf("ID igual ao do ultimo pacote recebido.\n");
+			continue;
+		}
+
+		if(pacote.flags == 64) // 0100 0000
+		{
+			// Pacote com bit END ligado
+			flag_end = true;
+		}
+
+		last_id = pacote.id;
+		last_chksum = pacote.chksum;
+
+		int writeSize = fwrite(pacote.data, sizeof(uint8_t), pacote.length, frecv);
+		if(writeSize < pacote.length)
+		{
+			fprintf(stderr, "ERROR on write file\n");
+		}
+		if(flag_end)
+			break;
+
+		// TEM QUE USAR HTONS AQUI!!!
+		memset(&ack, '\0', sizeof(packet_t));
+		ack.sync1 = SYNC;
+		ack.sync2 = SYNC;
+		ack.chksum = 0;
+		ack.length = 0;
+		ack.id = pacote.id;
+		ack.flags = 128; // 1000 0000
+		memset(ack.data, '\0', MAXDATASIZE);
+
+		ack.chksum = htons((uint16_t) csum((unsigned short*) &ack, sizeof(packet_t)));
+			
+		if(send(newSocketFD, (packet_t*) &ack, sizeof(packet_t), 0) < 0);
+		{
+			fprintf(stderr, "ERROR on sending ack. ID: %d\n", ack.id);
+		}
+	}
+	printf("Received from client\n");
+	fclose(frecv);
 
 	return 0;
 }
